ed8793306960279c583a59420d649503122c42d6
oterte <97093479+oterte@users.noreply.github.com>
2023-03-03 23:48:45 +0900

    사이드바 적용

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 
 
 // 전체 게시글 페이지
 function Board() {
   return (
-    <div>Board</div>
+    <>
+      <Sidebar></Sidebar>
+      <div>Board</div>
+    </>
   )
 }
 
